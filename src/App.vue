6b903c62b55dc93f56bd17140cf8e19fb51f5d3c
kashwa <kashwa80@gmail.com>
2018-12-07 14:26:47 +0200

    style App

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
-  <div id="app">
+  <div class="text-center" id="app">
     <h1>Word Translator.</h1>
     
-    <h5>Powered by Vue.js</h5>
+    <h5 class="text-muted">Powered by Vue.js</h5>
 
     <TranslateForm @formSubmit="translateText"></TranslateForm>
+
+    <TranslateOutput v-text="translatedText"></TranslateOutput>
   </div>
 </template>
 
 <script>
   import TranslateForm from './components/TranslateForm'
+  import TranslateOutput from './components/TranslateOutput'
 
   export default {
     name: 'App',
     components: {
-      TranslateForm
+      TranslateForm,
+      TranslateOutput
+    },
+    data: function () {
+      return {
+        translatedText: '',
+      }
     },
     methods: {
-      translateText(text) {
-        // alert(text);
+      translateText(text, lang) {
+        this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181207T073908Z.0a30ad49bc66a9e1.b14670e2e0c57468b2780be6bdbd02d74d5672b9&lang='+lang+'&text=' + text)
+        .then((response) => {
+          this.translatedText = response.body.text[0];
+        })
       }
     }
   }
 </script>
 
 <style>
-#app {
-  
+body {
+  background: #fefefe;
 }
 </style>
--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -1,11 +1,18 @@
 <template>
-  <div id="translateform">
-    
-    <form @submit="formSubmit">
-      <input type="text" v-model="textToTranslate" placeholder="Enter a word">
-      <input type="submit" value="translate">
-    </form>
+  <div class="row" id="translateform">
+    <div class="col-md-6 col-md-offset-3">
+      <form id="transform" class="form-inline well" @submit="formSubmit">
+        <input class="form-control" type="text" v-model="textToTranslate" placeholder="Enter a word">
+        <select class="form-control" v-model="lang">
+          <option value="ru">Russian</option>
+          <option value="es">Spanish</option>
+          <option value="ar">Arabic</option>
+          <option value="fr">French</option>
+        </select>
 
+        <input class="btn btn-primary" type="submit" value="translate">
+      </form>
+    </div>
   </div>
 </template>
 
@@ -15,12 +22,16 @@
     name: 'translateform',
     data() {
       return {
-        textToTranslate: ''
+        textToTranslate: '',
+        lang: ''
       }
+    },
+    created() {
+      this.lang = 'es'
     },
     methods: {
       formSubmit(e) {
-        this.$emit('formSubmit', this.textToTranslate);
+        this.$emit('formSubmit', this.textToTranslate, this.lang);
         e.preventDefault();
       }
     }
@@ -28,5 +39,14 @@
 </script>
 
 <style>
-
+  #transform {
+    border-radius: 10px;
+    border: 1px #ccc solid;
+  }
+  select, input {
+    margin-left: 10%;
+  }
+  h5 {
+    margin-bottom: 5%;
+  }
 </style>
