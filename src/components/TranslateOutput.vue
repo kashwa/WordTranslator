06841214d7bb9354381beb1a6f2a9fa22da50159
kashwa <kashwa80@gmail.com>
2018-12-07 14:26:34 +0200

    Output component

---
+++ src/components/TranslateOutput.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>{{ translatedText }}</h2>
+</template>
+
+<script>
+
+  export default {
+    name: 'translateOutput',
+    props: ['translatedText'],
+    data() {
+      return {
+        textToTranslate: ''
+      }
+    },
+    methods: {
+      formSubmit(e) {
+        this.$emit('formSubmit', this.textToTranslate);
+        e.preventDefault();
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
